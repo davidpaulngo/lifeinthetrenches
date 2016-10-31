cfa1b327ac463d0708568a82f79ecee7d5e058b2
David Paul Ngo <David Paul Ngo>
2016-10-31 21:48:33 +0800

    31-10-2016 - Added a feature to the function to merge the output to a single file. Suitable for copy functions in Azure RM.

--- ConvertTo-AzureRmParametersJson.ps1
+++ ConvertTo-AzureRmParametersJson.ps1
@@ -5,8 +5,8 @@
     .DESCRIPTION
         This function takes an input CSV file and for each line it extracts the properties 
         and wraps it into an Azure Resource Management template parameter file in JSON format. 
-        By default, the output filenames are of the following format: item-<count>.json. This 
-        function is useful in scenarios where it is necessary to provision multiple resources 
+        By default, the multiple output files are produced but it can also produce a single output file. 
+        This function is useful in scenarios where it is necessary to provision multiple resources 
         with the same set of parameters (with different values) in Azure.
 
     .PARAMETER CSVFile
@@ -14,10 +14,18 @@
         This parameter is mutually exclusive to the "InputObject" parameter.
     
     .PARAMETER OutputPrefix
-        This parameter is optional and it is used to specify the prefix prepended to the output files.
+        This parameter is used to specify the prefix prepended to the output files. If there are multiple output files, 
+        this prefix is appended with a running number that starts from 1. 
+
+    .PARAMETER MergeOutput
+        This paramether is used to determine whether there will be multiple output files or a single output file. By default, 
+        multiple output files are produced.
+
+    .PARAMETER ObjectLabel
+        This paramether is used to determine what the name of the parameter will be if there is only a single output file.
 
     .INPUTS
-        This function can take a PowerShell object from the pipeline.
+        This function can receive a file path from the pipeline.
 
     .OUTPUTS
         This function returns a collection of the filenames the JSON files produced.
@@ -44,21 +52,28 @@ function ConvertTo-AzureRmParametersJSON {
         [string] $CSVFile,
 
         [Parameter(ParameterSetName="CSVFile")]
-        [string] $OutputPrefix = "item-"
+        [string] $OutputPrefix = "item-",
+
+        [Parameter(ParameterSetName="CSVFile")]
+        [bool] $MergeOutput = $false,
+
+        [Parameter(ParameterSetName="CSVFile")]
+        [ValidateScript({$_.length -gt 0})]
+        [string] $ObjectLabel = "Objects"
     )
     
-    if($PSCmdlet.ParameterSetName -eq "CSVFile") {
+    try {
         $sourceObj = Import-Csv $CSVFile
     }
-    else {
-        $sourceObj = $null
+    catch [System.Exception] {
+         
     }
 
     $outputFiles = @()
-    $count = 0
+    $count = 1
     if($sourceObj) {
-        # Evaluate each line item in the CSV
-        foreach($item in $sourceObj) {
+
+        if($MergeOutput) {
 
             # Create new PowerShell object to store JSON data
             $newObj = New-Object System.Object
@@ -68,31 +83,73 @@ function ConvertTo-AzureRmParametersJSON {
                 "http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#"
             $newObj | Add-Member -Type NoteProperty -Name "contentVersion" -Value `
                 "1.0.0.0"
-            
-            # Evaluate each property of the CSV line item
-            $params = @{}
-            $item.PsObject.Properties | ForEach-Object {            
-                $name = $_.Name
-                $value = $_.Value
-                
-                $valueObj = @{}
-                $valueObj.Add("value",$value)
 
-                $params.Add($name,$valueObj)
+            $values = @()
+            # Evaluate each line item in the CSV
+            foreach($item in $sourceObj) {
+                $params = @{}
+
+                $item.PsObject.Properties | ForEach-Object {            
+                    $name = $_.Name
+                    $value = $_.Value
+                    $params.Add($name,$value)
+                }
+
+                $values += $params
             }
-
+            
+            $valueWrapper = @{"value" = $values}
+            $paramWrapper = @{$ObjectLabel = $valueWrapper}
+            
             # Add "parameters" property to the object
-            $newObj | Add-Member -Type NoteProperty -Name "parameters" -Value $params
-            
-            # Build output file name
-            $fileName = $OutputPrefix + "$count.json"
+            $newObj | Add-Member -Type NoteProperty -Name "parameters" -Value $paramWrapper
 
+            # Build output file name
+            $fileName = "$OutputPrefix.json"
             # Convert the PowerShell object to JSON and write the output file
-            ConvertTo-Json -InputObject $newObj | Out-File $fileName
-            
+            ConvertTo-Json -InputObject $newObj -Depth 20 | Out-File $fileName
+
             $outputFiles += $fileName
-            $count++
         }
+        else {
+            # Evaluate each line item in the CSV
+            foreach($item in $sourceObj) {
+
+                # Create new PowerShell object to store JSON data
+                $newObj = New-Object System.Object
+
+                # Add standard properties of the parameter file to the object
+                $newObj | Add-Member -Type NoteProperty -Name "`$schema" -Value `
+                    "http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#"
+                $newObj | Add-Member -Type NoteProperty -Name "contentVersion" -Value `
+                    "1.0.0.0"
+                
+                # Evaluate each property of the CSV line item
+                $params = @{}
+                $item.PsObject.Properties | ForEach-Object {            
+                    $name = $_.Name
+                    $value = $_.Value
+                    
+                    $valueObj = @{}
+                    $valueObj.Add("value",$value)
+
+                    $params.Add($name,$valueObj)
+                }
+
+                # Add "parameters" property to the object
+                $newObj | Add-Member -Type NoteProperty -Name "parameters" -Value $params
+                
+                # Build output file name
+                $fileName = $OutputPrefix + "$count.json"
+
+                # Convert the PowerShell object to JSON and write the output file
+                ConvertTo-Json -InputObject $newObj | Out-File $fileName
+                
+                $outputFiles += $fileName
+                $count++
+            }
+        }
+        
 
         # Return data of the script
         $outputFiles
